8f9f99e6b65566daa165db7d2c5863b5133ca1d2
Milos Markovic <milosmarkovic83@gmail.com>
2022-08-31 10:17:30 -0400

    added new line and Water class is completed

--- src/practice1/Water.java
+++ src/practice1/Water.java
@@ -4,4 +4,8 @@ public class Water {
     //created class water
 
     // chemical formula for water is H2O
+
+    //water is important for us to prevent dehydration
+
+    //class Water is completed
 }
